a56b28aecb0ac57c72f8acfb81123484e9b62388
Albob <alexis.bauchu@gmail.com>
2022-01-27 00:35:06 +0100

    Reduce number of files

---
+++ game.js
@@ -0,0 +1,13 @@
+const kAppVersion = "0.0.1";
+
+class Game {
+  constructor() {
+  }
+}
+
+function main() {
+  const game = new Game();
+  alert(`Bienvenue dans Coworking Simulator !`);
+}
+
+window.addEventListener('load', main)
--- main.js
+++
@@ -1,8 +0,0 @@
-import Game from "./modules/game.js"
-
-function main() {
-  const game = new Game();
-  alert(`Bienvenue dans Coworking Simulator ${Game.appVersion}`);
-}
-
-window.addEventListener('load', main)
