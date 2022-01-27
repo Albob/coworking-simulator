33f8f0410e777d3b40ba59d684ffc43ba1ad27fb
Albob <alexis.bauchu@gmail.com>
2022-01-27 01:00:39 +0100

    Handle 1 coworker that earns 1euro per hour

--- game.js
+++ game.js
@@ -1,13 +1,61 @@
 const kAppVersion = "0.0.1";
 
+const kCoworkerFirstNames = [
+  'Pierre',
+  'Pierre-Louis',
+  'Pierre-Francois',
+  'Sophie',
+  'Chloe',
+  'Nico',
+  'Gael',
+  'Sandra',
+  'Paul',
+  'David',
+  'Elise',
+  'Guillaume',
+  'Benjamin',
+  'Christine',
+  'Alexis',
+  'Alexandra',
+  'Emilie',
+  'Philippe',
+  'Anthony',
+];
+
+function randomElement(collection) {
+  const index = Math.floor(Math.random() * collection.length);
+  return collection[index];
+}
+
+const kCoworkerNameFirstLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+class Coworker {
+  constructor() {
+    this.name = `${randomElement(kCoworkerFirstNames)} ${randomElement(kCoworkerNameFirstLetters)}.`;
+  }
+}
+
 class Game {
   constructor() {
+    this.coworkers = [new Coworker()];
+    this.money = 0;
+  }
+
+  update() {
+    this.money += this.coworkers.length
+  }
+
+  render() {
+    document.getElementById('coworkers').innerText = `Nom: ${this.coworkers[0].name}`;
+    document.getElementById('money').innerText = this.money + "€";
   }
 }
 
 function main() {
   const game = new Game();
-  alert(`Bienvenue dans Coworking Simulator !`);
+  window.setInterval(() => {
+    game.update();
+    game.render();
+  }, 1000);
 }
 
 window.addEventListener('load', main)
