276ac222ffef82282a13008e3f8e561a2ec6ddf1
Albob <alexis.bauchu@gmail.com>
2022-01-29 01:29:11 +0100

    Generate random job titles

--- game.js
+++ game.js
@@ -22,31 +22,84 @@ const kPlaces = [
 ];
 
 const kCoworkerFirstNames = [
-  'Pierre',
-  'Pierre-Louis',
-  'Pierre-Francois',
-  'Sophie',
-  'Chloe',
-  'Nico',
-  'Gael',
-  'Sandra',
-  'Paul',
-  'David',
-  'Elise',
-  'Guillaume',
-  'Benjamin',
-  'Christine',
-  'Alexis',
-  'Alexandra',
-  'Emilie',
-  'Philippe',
-  'Anthony',
+  ['Pierre', 'M'],
+  ['Pierre-Louis', 'M'],
+  ['Pierre-Francois', 'M'],
+  ['Sophie', 'F'],
+  ['Chloe', 'F'],
+  ['Nico', 'M'],
+  ['Gael', 'M'],
+  ['Sandra', 'F'],
+  ['Paul', 'M'],
+  ['David', 'M'],
+  ['Elise', 'F'],
+  ['Guillaume', 'M'],
+  ['Benjamin', 'M'],
+  ['Christine', 'F'],
+  ['Alexis', 'M'],
+  ['Alexandra', 'F'],
+  ['Emilie', 'F'],
+  ['Philippe', 'M'],
+  ['Anthony', 'M'],
+];
+
+const kJobs = [
+  ['Influenceur', 'Influenceuse'],
+  ['Developpeur', 'Developpeuse'],
+  ['Architecte', 'Architecte'],
+  ['Graphiste', 'Graphiste'],
+  ['Facilitateur', 'Facilitatrice'],
+  ['Congierge', 'Congierge'],
+  ['Commercial', 'Commerciale'],
+  ['Vidéaste', 'Vidéaste'],
+  ['Agent immobilier', 'Agent immobilier'],
+  ['Formateur', 'Formatrice'],
+  ['Journaliste', 'Journaliste'],
+  ['Écrivain', 'Écrivain'],
+  ['Négociateur', 'Négociatrice'],
+  ['Agriculteur', 'Agricultrice'],
+  ['Conseiller', 'Conseillère'],
+  ['Éleveur', 'Éleveuse'],
+];
+
+const kJobAdjectives = [
+  ['', ''],
+  ["mais n'assume pas", "mais n'assume pas"],
+  ['en developpement durable', 'en developpement durable'],
+  ['pour mal entendants', 'pour mal entendants'],
+  ['pour personnes âgées', 'pour personnes âgées'],
+  ['pour animaux', 'pour animaux'],
+  ['paysagiste', 'paysagiste'],
+  ['des stars', 'des stars'],
+  ['en reconversion', 'en reconversion'],
+  ['en agencement', 'en agencement'],
+  ['sur internet', 'sur internet'],
+  ['en éducation', 'en éducation'],
+  ['autodidacte', 'autodidacte'],
+  ['debutant', 'debutante'],
+  ['confirmé', 'confirmée'],
+  ['retraité', 'retraitée'],
+  ['excentrique', 'excentrique'],
+  ['farfelu', 'farfelue'],
+  ['consultant', 'consultante'],
+  ['à mi-temps', 'à mi-temps'],
+  ['des temps modernes', 'des temps modernes'],
+  ["mais rêve secrètement d'être aventurier", "mais rêve secrètement d'être aventurière"],
 ];
 
 const kCoworkerNameFirstLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
 class Coworker {
   constructor() {
-    this.name = `${randomElementIn(kCoworkerFirstNames)} ${randomElementIn(kCoworkerNameFirstLetters)}.`;
+    const firstNameAndSex = randomElementIn(kCoworkerFirstNames);
+    const firstName = firstNameAndSex[0];
+    const sex = firstNameAndSex[1];
+
+    this.name = `${firstName} ${randomElementIn(kCoworkerNameFirstLetters)}.`;
+
+    const jobTitle = randomElementIn(kJobs)[sex == 'M' ? 0 : 1];
+    const jobAdjective = randomElementIn(kJobAdjectives)[sex == 'M' ? 0 : 1];
+    this.job = `${jobTitle} ${jobAdjective}`;
   }
 }
 
@@ -67,7 +120,11 @@ class Game {
 
     const place = kPlaces[this.level];
     this.dom.place.innerHTML = `Nom: ${place.name}<br/>Description: ${place.description}`;
-    this.dom.coworkers.innerText = `Nom: ${this.coworkers[0].name}`;
+
+    this.dom.coworkers.innerHTML = '';
+    this.coworkers.forEach(coworker => {
+      this.dom.coworkers.innerHTML = `Nom: ${coworker.name}<br/>Métier: ${coworker.job}`;
+    });
   }
 
   update() {
